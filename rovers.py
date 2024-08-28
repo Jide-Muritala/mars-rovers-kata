ee61b2a508f69e7970813d20df4ceb51c92f53af
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2024-08-28 12:37:34 +0000

    Pending changes exported from your codespace

---
+++ rovers.py
@@ -0,0 +1,18 @@
+class Rover:
+    def __init__(self, x, y, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.directions = ['N', 'E', 'S', 'W']
+
+    def turn_left(self):
+        pass
+
+    def turn_right(self):
+        pass
+
+    def move_forward(self):
+        pass
+
+    def move_backward(self):
+        pass
